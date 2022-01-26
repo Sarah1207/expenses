c8c86137a311157edf61f56dd4408d50c08ea4f6
Sarah HAMI <sarah.hami07@gmail.com>
2022-01-26 13:55:57 +0100

    new arbo for components

--- src/components/Expenses/ExpensesChart/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart/ExpensesChart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Chart from '../Chart/Chart';
+import Chart from '../../../components/Chart/Chart/Chart';
 
 const ExpensesChart = (props) => {
   const chartDataPoints = [
